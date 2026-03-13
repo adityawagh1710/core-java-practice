9600a5aa53493b967bc2d71fadd2f733bbb1e9d4
Aditya Wagh <ad.wagh@easternenterprise.com>
2026-03-13 15:45:02 +0530

    feat: care java practice examples for equals hash code override

---
+++ src/com/practice/collections/EqualsHashcodeOverrideExample.java
@@ -0,0 +1,45 @@
+package com.practice.collections;
+
+import java.util.Objects;
+
+class Student {
+	
+	int id;
+	String name;
+	
+	Student(int id,String name){
+		this.id = id;
+		this.name = name;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		
+		if(this == obj)
+			return true;
+		
+		if(obj == null || getClass() != obj.getClass())
+			return false;
+		
+		Student s = (Student) obj;
+		
+		return id == s.id;
+	}
+	
+	@Override
+	public int hashCode(){
+		return Objects.hash(id);
+	}
+}
+
+public class EqualsHashcodeOverrideExample {
+	
+	public static void main(String[] args){
+		Student s1 = new Student(1,"Aditya");
+		Student s2 = new Student(1,"Shivam");
+		
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+	}
+}
